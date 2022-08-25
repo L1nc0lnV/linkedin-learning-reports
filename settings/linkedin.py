03a59b2afb5fc66c3b54c94b9ca21a953691e1b8
L1nc0lnV <jt@rt250.com>
2022-08-25 08:30:36 -0400

    Pre-Azure Function Rollout

--- settings/linkedin.py
+++ settings/linkedin.py
@@ -13,9 +13,9 @@ class LinkedInConfig:
             self.CLIENT_SECRET = fi.readline().rstrip('\n')
         self.CLIENTID = '77plycpp70x634'
         self.TOKEN = LinkedInConfig.GetLinkedinRefreshToken(self, self.CLIENT_SECRET, self.CLIENTID)
-        self.MEASURE_PERIOD = 'DAY'  # Day or Week
+        self.MEASURE_PERIOD = 'WEEK'  # Day or Week
         self.MEASURE_PERIOD_LOOKAHEAD = 1  # In days, can be a max of 14 days whether using DAY (14) or WEEK (2)
-        self.MEASURE_PERIOD_START = round((datetime.now() + timedelta(-1)).timestamp() * 1000)  # epoch datetime now
+        self.MEASURE_PERIOD_START = round((datetime.now() + timedelta(-7)).timestamp() * 1000)  # epoch datetime now
         self.PAGE_COUNT = 1000
         self.PAGE_START = 0
         self.ASSET_TYPE = ['COURSE', 'VIDEO', 'ARTICLE', 'BOOK', 'EVENT', 'LEARNING_COLLECTION', 'LEARNING_PATH',
